78c117f80f628a2dae93b3f413e54057e9e4153b
kishore-narendran <kishore.narendran09@gmail.com>
2014-09-21 13:27:22 +0530

    Mongodb connection and test url call

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var mongoClient = require('mongodb').MongoClient;
+var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/VITacademics';
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+    var name = req.param('name');
+    var onInsert = function(err,records){
+        console.log(records[0].name);
+    };
+    var onConnect = function(err, db) {
+        if(err){}
+        else{
+            db.collection('names').insert({name:name}, onInsert);
+        }
+    };
+    mongoClient.connect(mongoUri, onConnect);
+    res.render('index', { title: 'Hello World!', name: name });
 });
 
 module.exports = router;
